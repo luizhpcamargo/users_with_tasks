fe0272daca438042c1fade4e568e3946ff2e8f29
Luiz Camargo <luizhp.camargo@gmail.com>
2018-02-27 13:06:04 -0300

    User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :tasks
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates_format_of :email, with: VALID_EMAIL_REGEX
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,35 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { expect(have_many(:tasks)).to be_truthy }
 
-  describe '#password' do
+  context '#password' do
     let (:user) {User.new(password: 'initial_password', password_confirmation: 'initial_password')}
     it { expect(user.password_digest.present?).to be_truthy}
     it { expect(user.password_digest).to_not be 'initial_password' }
+  end
+
+  context 'password presence' do
+    let(:user) { User.new(email: 'test@valid.com') }
+
+    it do
+      expect(user.valid?).to be_falsey
+      expect(user.errors[:password]).to include 'can\'t be blank'
+    end
+
+    it do
+      user.password = 'test'
+      expect(user.valid?).to be_truthy
+    end
+  end
+
+  context 'email validation' do
+    let(:valid_email) { User.new(email: 'test@email.com', password: 'checking') }
+    let(:invalid_email) { User.new(email: '@@test', password: 'checking') }
+
+    it { expect(valid_email.valid?).to be_truthy }
+
+    it do
+      expect(invalid_email.valid?).to be_falsey
+      expect(invalid_email.errors[:email]).to include 'is invalid'
+    end
   end
 end
