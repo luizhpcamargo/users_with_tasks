f40c597559f2c7a0fae44759606fbc9ca9d4df0d
Luiz Camargo <luizhp.camargo@gmail.com>
2018-02-27 12:24:42 -0300

    Forgot to include the users index :see_no_evil:

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20180227142434) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email"
   end
 
   add_foreign_key "tasks", "users"
