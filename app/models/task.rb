7683a53ea7cc7eff11553101ec11a98ea8dfafa5
Luiz Camargo <luizhp.camargo@gmail.com>
2018-02-27 12:58:53 -0300

    Tasks validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,13 @@
 class Task < ApplicationRecord
   belongs_to :user
+
+  validates_presence_of :description
+  validate :valid_schedule?
+
+  protected
+
+  def valid_schedule?
+    return if start_date.nil? || end_date.nil?
+    errors.add(:schedule, "is not valid") if start_date > end_date
+  end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,6 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  let (:task) { Task.new }
+  let (:task) { Task.new(user: User.new) }
   it { expect(task.build_user).to be_a(User) }
+
+  context 'description' do
+    before do
+      task.description = nil
+      task.valid?
+    end
+    it { expect(task.valid?).to be_falsey }
+    it { expect(task.errors[:description]).to include 'can\'t be blank' }
+  end
+
+  context '#valid_schedule?' do
+    before do
+      task.description = 'I\'m a test'
+    end
+    context 'invalid schedule' do
+      before do
+        task.start_date = 1.day.ago
+        task.end_date = 2.days.ago
+        task.valid?
+      end
+
+      it { expect(task.valid?).to be_falsey }
+      it { expect(task.errors[:schedule]).to include 'is not valid' }
+    end
+
+    context 'with no schedule' do
+      before do
+        task.start_date = nil
+        task.end_date = nil
+      end
+
+      it { expect{task.valid?}.to_not raise_error }
+    end
+
+    context 'valid schedule' do
+      before do
+        task.start_date = 2.days.ago
+        task.end_date = 2.days.since
+        task.valid?
+      end
+
+      it { expect(task.valid?).to be_truthy }
+    end
+  end
+
+
 end
